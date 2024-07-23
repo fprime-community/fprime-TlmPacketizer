b02b57d02aea6f58f52cd17609913f9f39a6d2ac
Ali Mosallaei <ali.mosallaei@jpl.nasa.gov>
2024-07-23 09:25:37 -0700

    Update to work with tool v1.1.2

--- Telemetry/TelemetryTopologyDefs.hpp
+++ Telemetry/TelemetryTopologyDefs.hpp
@@ -7,9 +7,6 @@ struct TelemetryState {
     /* include any variables that are needed for 
     configuring/starting/tearing down the topology */
 };
-struct TopologyState {
-    TelemetryState Telemetry_state;
-};
 
 namespace GlobalDefs {
     namespace PingEntries {
